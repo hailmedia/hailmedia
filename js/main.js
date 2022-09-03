bd4d629cfa43b2732337258ee2a35f70bab1b783
ashrafShaaban <ashrafshaaban37@gmail.com>
2022-09-03 11:31:35 -0700

    The Whole Project Files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let myoverlay= document.querySelector(".overlay");
+
+function removeoverlay(){
+    myoverlay.remove();
+}
+setTimeout(removeoverlay,3500)
+// let myimg = document.querySelector(".isrc");
+// console.log(myimg.src);
+
+
+//     let myover=document.createElement("div");
+//     myover.className="overlay";
+//     let myimg2=document.createElement("img")
+//     myimg2.src=myimg.src;
+//     myover.appendChild(myimg2);
+
+//     document.body.appendChild(myover);
+
+
+// // window.onload = function(){
+// //     let mySetover= setTimeout(over,1000);
+// // };
+
